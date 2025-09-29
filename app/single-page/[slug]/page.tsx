e01820efe24e6825d7e32a319b8f6085e61951ec
widura26 <widurategalrejo@gmail.com>
2025-09-29 10:24:55 +0700

    fix(route): handle null params in dynamic page

--- app/single-page/[slug]/page.tsx
+++ app/single-page/[slug]/page.tsx
@@ -6,7 +6,7 @@ export default function DynamicPage() {
     return (
         <div className='text-white'>
             <h1>Halaman Dinamis</h1>
-            <p>Slug: {params.slug}</p>
+            <p>Slug: {params?.slug}</p>
         </div>
     )
 }
